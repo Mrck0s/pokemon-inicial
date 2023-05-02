797cf8500ab177491b5f06640e0c73911a6a1aff
Mrck0s <110942627+Mrck0s@users.noreply.github.com>
2023-05-02 11:48:22 -0300

    v1
    proyecto terminado

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const pkbBul = document.querySelector("#pkb-bul");
+const pkbSqui = document.querySelector("#pkb-squi");
+const pkbChar = document.querySelector("#pkb-char");
+const popupContainer = document.querySelector(".popup-container");
+const popupBul = document.querySelector(".bulbasaur");
+const popupSqui = document.querySelector(".squirtle");
+const popupChar = document.querySelector(".charmander");
+const selections = document.querySelectorAll(".selection-option");
+
+const bulbasaurCries = ["/src/bulbasaur-cry-1.mp3", "/src/bulbasaur-cry-2.mp3", "/src/bulbasaur-cry-3.mp3"];
+const squirtleCries = ["/src/squirtle-cry-1.mp3", "/src/squirtle-cry-2.mp3", "/src/squirtle-cry-3.mp3"];
+const charmanderCries = ["/src/charmander-cry-1.mp3", "/src/charmander-cry-2.mp3", "/src/charmander-cry-3.mp3"];
+
+pkbBul.addEventListener("click", () =>{
+  const soundIndex = Math.floor(Math.random() * 3);
+  const sound = new Audio(bulbasaurCries[soundIndex]);
+  sound.play();
+
+  popupContainer.style.display = "flex";
+  popupBul.style.display = "flex";
+})
+pkbSqui.addEventListener("click", () =>{
+  const soundIndex = Math.floor(Math.random() * 3);
+  const sound = new Audio(squirtleCries[soundIndex]);
+  sound.play();
+
+  popupContainer.style.display = "flex";
+  popupSqui.style.display = "flex";
+})
+pkbChar.addEventListener("click", () =>{
+  const soundIndex = Math.floor(Math.random() * 3);
+  const sound = new Audio(charmanderCries[soundIndex]);
+  sound.play();
+
+  popupContainer.style.display = "flex";
+  popupChar.style.display = "flex";
+})
+
+selections.forEach(selection => {
+  selection.addEventListener("click", () => {
+    popupContainer.style.display = "none";
+    popupBul.style.display = "none";
+    popupSqui.style.display = "none";
+    popupChar.style.display = "none";
+  })
+});
